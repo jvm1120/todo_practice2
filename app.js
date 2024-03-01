da14de861d3b84801d19439fb3388017f5000122
jvm1120 <flowercord@gmail.com>
2024-03-01 10:27:13 +0900

    div追加

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-$("#addTodo").click(function(){
-    const inputTodo = $("input").val();
-    $("#todoList").append("<li><input type='checkbox'>"+ inputTodo + "</li>");
-    $("input").val("");
-});
+// $("#addTodo").click(function(){
+//     const inputTodo = $("input").val();
+//     $("#todoList").append("<li><input type='checkbox'>"+ inputTodo + "</li>");
+//     $("input").val("");
+// });
+
+
+const box = document.getElementById('test');
+
+for (i = 0; i < 10; i++){
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "child");
+    const hue = i * (360 / 10); 
+    newDiv.style.backgroundColor = `hsl(${hue}, 60%, 60%)`;
+    box.appendChild(newDiv);
+}
+
+
+
